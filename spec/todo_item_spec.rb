eab5e9bda47349bf761e7c7de40592b01949e4c5
girish-gh <ilogix21@outlook.com>
2022-02-18 15:42:07 +0530

    RSpec test files added

---
+++ spec/todo_item_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+
+RSpec.describe TodoItem, :type => :model do
+
+  it "has none to begin with" do
+    expect(TodoItem.count).to eq 0
+  end
+
+  it "has one after adding one" do
+    todolist =  TodoList.create(title: "title",description: "description",created_at:"01-01-2020",updated_at:"01-01-2020")
+    todoItem =  todolist.todo_items.create(name: "foo",isRecurring: true,created_at:"01-01-2020",updated_at:"01-01-2020")
+    expect(TodoItem.count).to eq 1
+  end
+
+  it "has less by one after deleting one" do
+    countb4Add = TodoItem.count
+    todolist =  TodoList.create(title: "title",description: "description",created_at:"01-01-2020",updated_at:"01-01-2020")
+    todoItem =  todolist.todo_items.create(name: "foo",isRecurring: true,created_at:"01-01-2020",updated_at:"01-01-2020")
+    countAftAdd = TodoItem.count   
+    todoItem.destroy   
+    countb4Add = countAftAdd-1
+    expect(TodoItem.count).to eq countb4Add
+  end
+
+
+  it "has none after one was created in a previous example" do
+    expect(TodoItem.count).to eq 0
+  end
+
+  context 'validation test' do
+    it 'ensures name presence' do
+      todolist =  TodoList.create(title: "title",description: "description",created_at:"01-01-2020",updated_at:"01-01-2020")
+    todoItem =  todolist.todo_items.create(isRecurring: true,created_at:"01-01-2020",updated_at:"01-01-2020")
+      expect(todoItem).to raise_error("NotNullViolation")
+  end
+  end
+
+    context 'validation test' do
+      it 'ensures isRecurring presence' do
+        todolist =  TodoList.create(title: "title",description: "description",created_at:"01-01-2020",updated_at:"01-01-2020")
+        todoItem =  todolist.todo_items.create(name: "foo",created_at:"01-01-2020",updated_at:"01-01-2020")
+        expect(todoItem).to raise_error("NotNullViolation")
+    end
+  end
+
+
+end
---
+++ spec/todo_list_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+
+RSpec.describe TodoList, :type => :model do
+
+  it "has none to begin with" do
+    expect(TodoList.count).to eq 0
+  end
+
+  it "has one after adding one" do
+    todolist =  TodoList.create(title: "title",description: "description",created_at:"01-01-2020",updated_at:"01-01-2020")
+    expect(TodoList.count).to eq 1
+  end
+
+  it "has less by one after deleting one" do
+    count = TodoList.count
+    todolist =  TodoList.create(title: "wierd",description: "description",created_at:"01-01-2020",updated_at:"01-01-2020")
+    count1 = TodoList.count   
+    todolist.destroy   
+    expect(TodoList.count).to eq count=count1-1
+  end
+
+
+  it "has none after one was created in a previous example" do
+    expect(TodoList.count).to eq 0
+  end
+
+  context 'validation test' do
+      it 'ensures title presence' do
+        todolist =  TodoList.create(description: "description",created_at:"01-01-2020",updated_at:"01-01-2020")
+        p todolist       
+        expect{todolist}.to raise_error("NotNullViolation")
+    end
+  end
+
+  context 'validation test' do
+    it 'ensures description presence' do
+      todolist =  TodoList.create(title: "title",created_at:"01-01-2020",updated_at:"01-01-2020")
+      p todolist       
+      expect{todolist}.to raise_error("NotNullViolation")
+  end
+end
+
+end
